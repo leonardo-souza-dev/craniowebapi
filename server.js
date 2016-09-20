4b206e6d4f855a21034f34f530ab4faad2d01a9e
Usuário Convidado <Guest@PA06MICRO17.local>
2016-09-19 22:40:32 -0300

    server http

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var http = require('http');
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
                                         host     : 'us-cdbr-iron-east-04.cleardb.net/heroku_9295fbed090e56c',
@@ -6,10 +7,20 @@ var connection = mysql.createConnection({
                                         });
 //mysql://bcadada6a126f7:bfe1febc@us-cdbr-iron-east-04.cleardb.net/heroku_9295fbed090e56c?reconnect=true
 connection.connect();
-
+var asd = 'asd';
 connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-                 if (err) throw err;
+                 
+                 if (err) {
+                     console.log('E N T R O U   N O   E R R O  ');
+                     throw err;
+                 }
+                 asd = rows[0].solution;
                  console.log('The solution is: ', rows[0].solution);
-                 });
+});
 
 connection.end();
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n' + asd);
+}).listen();
