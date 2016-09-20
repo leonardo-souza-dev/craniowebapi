3f1d805929dcad97352f8e8f3a4bafdc1e2c7fa3
Usuário Convidado <Guest@PA06MICRO17.local>
2016-09-19 22:56:59 -0300

    server teste

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http');
-var mysql      = require('mysql');
+var mysql = require('mysql');
 var connection = mysql.createConnection('mysql://bcadada6a126f7:bfe1febc@us-cdbr-iron-east-04.cleardb.net/heroku_9295fbed090e56c?reconnect=true');
+
 /*
 var connection = mysql.createConnection({
                                         host     : 'us-cdbr-iron-east-04.cleardb.net/heroku_9295fbed090e56c',
@@ -10,20 +11,25 @@ var connection = mysql.createConnection({
  */
 //mysql://bcadada6a126f7:bfe1febc@us-cdbr-iron-east-04.cleardb.net/heroku_9295fbed090e56c?reconnect=true
 connection.connect();
+
 var asd = 'asd';
+
 connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
                  
-                 if (err) {
-                     console.log('E N T R O U   N O   E R R O  ');
-                     throw err;
-                 } else {
-                     console.log('---nao deu erro--');
-                     asd = rows[0].solution;
-                     console.log('The solution is: ', rows[0].solution);
-                 }
+    if (err) {
+        console.log('E N T R O U   N O   E R R O  ');
+        throw err;
+    } else {
+        console.log('---nao deu erro--');
+        asd = rows[0].solution;
+        console.log('The solution is: ', rows[0].solution);
+    }
 });
 
 connection.end();
+
+
+console.log('A SOLUCAO EH: ', asd);
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
